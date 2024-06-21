7d79a32825ff5366cf2817bdcce2941161b8ae83
Nathan Barros <nathansousa0809@gmail.com>
2024-06-21 16:55:52 -0300

    Service para projects

--- backend/src/modules/projects/projects.service.ts
+++ backend/src/modules/projects/projects.service.ts
@@ -1,5 +1,48 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/sequelize';
+import { Projects } from 'src/models/projects.model';
+import { Squads } from 'src/models/squads.model';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+    constructor(
+        @InjectModel(Projects) private readonly projectsModel: typeof Projects
+    ){}
+
+    async findAll(squadId: number): Promise< boolean | Projects[]> {
+        try {
+            return this.projectsModel.findAll({
+               include: [{
+                model: Squads,
+                required: true,
+                where: {
+                    idSquad: squadId
+                }
+               }]
+            });
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async find(id: number): Promise<Projects | boolean> {
+        try {
+            return this.projectsModel.findOne({
+                where: { idProject: id }
+            });
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async update(id: number, project: Projects): Promise<[affectedCount: number] | boolean> {
+        try {
+            return this.projectsModel.update(project, { where: { idProject: id } });
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+}
