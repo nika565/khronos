ea70a589f01787091dfe2b172ad9fcbb2111f55f
Nathan Barros <nathansousa0809@gmail.com>
2024-06-23 20:53:32 -0300

    Implementando Service de squads e tasks

--- backend/src/modules/squads/squads.service.ts
+++ backend/src/modules/squads/squads.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Squads } from 'src/models/squads.model';
+import { Users } from 'src/models/users.model';
 
 @Injectable()
-export class SquadsService {}
+export class SquadsService {
+    constructor(
+        @InjectModel(Squads) private readonly squadsModel: typeof Squads
+    ){}
+
+    async findAll(userId: number): Promise<Squads[] | boolean>{
+        try {
+            return await this.squadsModel.findAll({
+                include: [{
+                    model: Users,
+                    required: true,
+                    where: {
+                        fkIdUsers: userId,
+                    }
+                }]
+            });
+        } catch (error) {
+            console.log(error)
+            return false;
+        }
+    }
+
+    async find(id: number): Promise<Squads | boolean> {
+        try {
+            return await this.squadsModel.findOne({
+                where: { idSquad: id }
+            });
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async update(squad: Squads, id: number ) {
+        try {
+            return await this.squadsModel.update(squad, { where: { idSquad: id } })
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    };
+}
--- backend/src/modules/tasks/tasks.service.ts
+++ backend/src/modules/tasks/tasks.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Projects } from 'src/models/projects.model';
+import { Tasks } from 'src/models/tasks.model';
+import { Users } from 'src/models/users.model';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(
+        @InjectModel(Tasks) private readonly tasksModel: typeof Tasks
+    ){}
+
+    async findAllTasksUser(idUser: number): Promise<boolean | Tasks[]> {
+        try {
+            return await this.tasksModel.findAll({
+                include: [{
+                    model: Users,
+                    required: true,
+                    where: {
+                        fkIdUsers: idUser
+                    }
+                }]
+            });
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async findAllTasksProject(idProject: number): Promise<boolean | Tasks[]> {
+        try {
+            return await this.tasksModel.findAll({
+                include: [{
+                    model: Projects,
+                    required: true,
+                    where: {
+                        fkIdProjects: idProject
+                    }
+                }]
+            });
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async findOneTask(idTask: number): Promise<boolean | Tasks> {
+        try {
+            return await this.tasksModel.findOne({
+                where: { idTask: idTask }
+            });
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async update(task: Tasks, idTask: number): Promise<boolean | [affectedCount: number]> {
+        try {
+            return await this.tasksModel.update(task, {
+                where: { idTask: idTask }
+            })
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+}
